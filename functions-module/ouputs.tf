a65ac5043ea9b73a209653a27ededfe4af67d4ed
JDiego-softtek <juand.macias@softtek.com>
2026-04-29 14:50:50 -0600

    Create ouputs.tf

---
+++ functions-module/ouputs.tf
@@ -0,0 +1,31 @@
+output "ids" {
+  description = "Function App resource IDs by key"
+  value = {
+    for k, v in azurerm_linux_function_app.this :
+    k => v.id
+  }
+}
+
+output "names" {
+  description = "Function App names by key"
+  value = {
+    for k, v in azurerm_linux_function_app.this :
+    k => v.name
+  }
+}
+
+output "principal_ids" {
+  description = "Managed Identity principal IDs by Function App"
+  value = {
+    for k, v in azurerm_linux_function_app.this :
+    k => v.identity[0].principal_id
+  }
+}
+
+output "hostnames" {
+  description = "Default hostnames by Function App"
+  value = {
+    for k, v in azurerm_linux_function_app.this :
+    k => v.default_hostname
+  }
+}
